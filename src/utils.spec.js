a0b3767ba191a5af14aba5fb354b0ee6148aa608
Christopher Shelley <dont@forgetmy.email>
2022-11-28 11:18:47 +0100

    add "minus" func and test

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,8 +1,14 @@
 
-import add from './utils';
+import {add, minus} from './utils';
 
 describe('add', () => {
     it('adds two numbers', () => {
         expect(add(1, 2)).toBe(3);
+    })
+})
+
+describe('minus', () => {
+    it('minus two numbers', () => {
+        expect(minus(2, 1)).toBe(1);
     })
 })
